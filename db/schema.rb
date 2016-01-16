0d748a306ea8a256c0634c9cd41d2ecf544b8574
nnittop909 <nnittop_909@live.com>
2016-01-16 12:55:18 +0800

    sixth commit

--- db/schema.rb
+++ db/schema.rb
@@ -159,12 +159,6 @@ ActiveRecord::Schema.define(version: 20160116024949) do
     t.datetime "updated_at",    null: false
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "uploads", force: :cascade do |t|
     t.integer  "project_id"
     t.datetime "created_at",                 null: false
